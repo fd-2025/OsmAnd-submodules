f7d515a6523d4153870a2719089e61c923f38616
Mauersprinter <2800869-Mauersprinter@users.noreply.gitlab.com>
2025-07-12 19:35:18 +0200

    Update submodules to 5.1.7 [build#279], update Docker build and readmes

--- docker/setup-volume.sh
+++ docker/setup-volume.sh
@@ -9,9 +9,7 @@ git submodule update --depth 1
 rm -Rf android/OsmAnd-java/libs/*.jar
 rm -Rf android/OsmAnd/libs/*.jar
 rm -Rf android/OsmAnd-telegram/
-rm -Rf help/website/images/features.zip
 rm -Rf resources/icons/tools/SVGtoXML/vd-tool
-rm -Rf resources/test-resources/
 popd
 
 echo 'Proceed as described in the README.'
