cabec0804932947c42467d8bc63c5ed5d522ae6c
fd-2025 <217896674+fd-2025@users.noreply.github.com>
2025-10-29 21:42:01 +0100

    merge

--- build.sh
+++ build.sh
@@ -27,24 +27,24 @@ pushd core/wrappers/android/
 # build, assemble, assemble so that native libs are included
 gradle build
 gradle assembleRelease
-gradle assembleRelease
+#gradle assembleRelease
 cp build/outputs/aar/OsmAndCore_android-release.aar "$osmand_dir/libs/"
 cp NativeCoreRelease/build/outputs/aar/OsmAndCore_androidNativeRelease-release.aar "$osmand_dir/libs/"
 popd
 
-if  [[ "$native_lib_arch" == "armv7" ]]
-then
-    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-elif  [[ "$native_lib_arch" == "arm64" ]]
-then
-    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-else
-    #x86 or not specified/matching
-    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-fi
+#if  [[ "$native_lib_arch" == "armv7" ]]
+#then
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
+#elif  [[ "$native_lib_arch" == "arm64" ]]
+#then
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
+#else
+#    #x86 or not specified/matching
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
+#    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
+#fi
 
 # Build OsmAnd patched version of ICU. Remove a bunch of unused data
 # files to keep file size down. Copy into OsmAnd lib dirs.
