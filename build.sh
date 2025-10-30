f97f0cbbc4332162d90a202e96dc79ee28b61ec4
fd-2025 <217896674+fd-2025@users.noreply.github.com>
2025-10-30 10:48:36 +0100

    Update submodules to 5.2.7

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ osmand_java_dir="$script_dir/android/OsmAnd-java"
 # Build MP Android Chart
 
 pushd MPAndroidChart
-gradle assembleRelease
+gradle build
 cp MPChartLib/build/outputs/aar/MPChartLib-release.aar "$osmand_dir/libs/"
 popd
 
@@ -26,25 +26,9 @@ popd
 pushd core/wrappers/android/
 # build, assemble, assemble so that native libs are included
 gradle build
-gradle assembleRelease
-gradle assembleRelease
 cp build/outputs/aar/OsmAndCore_android-release.aar "$osmand_dir/libs/"
 cp NativeCoreRelease/build/outputs/aar/OsmAndCore_androidNativeRelease-release.aar "$osmand_dir/libs/"
 popd
-
-if  [[ "$native_lib_arch" == "armv7" ]]
-then
-    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-armeabi-v7a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-elif  [[ "$native_lib_arch" == "arm64" ]]
-then
-    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-arm64-v8a.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-else
-    #x86 or not specified/matching
-    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroid.jar "$osmand_dir/libs/"
-    cp core/externals/qtbase-android/upstream.patched.android.clang-x86.shared/jar/QtAndroidBearer.jar "$osmand_dir/libs/"
-fi
 
 # Build OsmAnd patched version of ICU. Remove a bunch of unused data
 # files to keep file size down. Copy into OsmAnd lib dirs.
