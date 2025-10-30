50c1133f2944a079cd83ffae13c40954acc7feac
fd-2025 <217896674+fd-2025@users.noreply.github.com>
2025-10-30 20:11:19 +0100

    Update submodules to 5.2.7 only 2x assembleRelease, fix

--- build.sh
+++ build.sh
@@ -41,8 +41,8 @@ echo "Core assembleRelease dry run END"
 #ls -la NativeCore*/build/outputs/aar/*.aar
 gradle assembleRelease
 echo "list core aar assembleRelease #1:"
-els -la build/outputs/aar/*.aar
-els -la NativeCore*/build/outputs/aar/*.aar
+ls -la build/outputs/aar/*.aar
+ls -la NativeCore*/build/outputs/aar/*.aar
 gradle assembleRelease
 echo "list core aar assembleRelease #2:"
 ls -la build/outputs/aar/*.aar
