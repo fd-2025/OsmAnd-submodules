a09a8957e51f496ade919030d76f406157fad9a4
fd-2025 <217896674+fd-2025@users.noreply.github.com>
2025-11-03 12:03:19 +0100

    fix build dependencies #11 final #2

--- build.sh
+++ build.sh
@@ -17,45 +17,14 @@ osmand_java_dir="$script_dir/android/OsmAnd-java"
 # Build MP Android Chart
 
 pushd MPAndroidChart
-echo "MPAndroidChart assembleRelease dry run BEGIN"
-gradle assembleRelease --dry-run
-echo "MPAndroidChart assembleRelease dry run END"
 gradle assembleRelease
-echo "list MPAndroidChart aar:"
-ls -la MPChartLib/build/outputs/aar/*.aar
 cp MPChartLib/build/outputs/aar/MPChartLib-release.aar "$osmand_dir/libs/"
 popd
 
 # Build OsmAnd core and copy into libs folder
 
 pushd core/wrappers/android/
-echo "Core build dry run BEGIN"
-gradle build --dry-run
-echo "Core build dry run END"
-##echo "Core build dry run -x BEGIN"
-##gradle build -x :OsmAndCore_androidNativeDebug:build --dry-run
-##echo "Core build dry run -x END"
-
-# build, assemble so that native libs are included
-##gradle build -x :OsmAndCore_androidNativeDebug:build
 gradle build
-echo "list core aar without assembleRelease:"
-ls -la build/outputs/aar/*.aar
-ls -la NativeCore*/build/outputs/aar/*.aar
-
-echo "Core assembleRelease dry run BEGIN"
-gradle assembleRelease --dry-run
-echo "Core assembleRelease dry run END"
-echo "Core assembleRelease dry run -x BEGIN"
-#gradle assembleRelease -x :OsmAndCore_androidNativeDebug:assembleRelease -x :swigGenerateJava -x :OsmAndCore_androidNative:buildOsmAndCore --dry-run
-gradle assembleRelease -x :swigGenerateJava -x :OsmAndCore_androidNative:buildOsmAndCore --dry-run
-echo "Core assembleRelease dry run -x END"
-
-#gradle assembleRelease -x :OsmAndCore_androidNativeDebug:assembleRelease -x :swigGenerateJava -x :OsmAndCore_androidNative:buildOsmAndCore
-########gradle assembleRelease -x :swigGenerateJava -x :OsmAndCore_androidNative:buildOsmAndCore
-echo "list core aar assembleRelease:"
-ls -la build/outputs/aar/*.aar
-ls -la NativeCore*/build/outputs/aar/*.aar
 cp build/outputs/aar/OsmAndCore_android-release.aar "$osmand_dir/libs/"
 cp NativeCoreRelease/build/outputs/aar/OsmAndCore_androidNativeRelease-release.aar "$osmand_dir/libs/"
 popd
